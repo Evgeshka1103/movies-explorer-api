fd0f011c2c7f0de9fbbe2b86a89be80c5fb02395
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-02-11 12:40:14 +0300

    Удаляет конфликтующие файлы

--- middlewares/config.js
+++
@@ -1,5 +0,0 @@
-const configUrl = {
-  db_url: 'mongodb://127.0.0.1:27017/bitfilmsdb',
-};
-
-module.exports = configUrl;
--- middlewares/error.js
+++
@@ -1,9 +0,0 @@
-const handleError = (err, req, res, next) => {
-  const { InternalServerError = 500, message } = err;
-  res.status(InternalServerError).send({
-    message:
-      InternalServerError === 500 ? 'Внутренняя ошибка сервера' : message,
-  });
-  next();
-};
-module.exports = { handleError };
