3126fe6bad08a5f30110e42e8d877daa9ce26aa1
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-02-13 21:53:34 +0300

    Исправляет централизованный обработчик

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,8 +1,8 @@
 const handleError = (err, req, res, next) => {
-  const { status = 500, message } = err;
-  console.log(message);
-  res.status(status).send({
-    message: status === 500 ? 'Внутренняя ошибка сервера' : message,
+  const { statusCode = 500, message } = err;
+  console.log(err);
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'Внутренняя ошибка сервера' : message,
   });
   next();
 };
